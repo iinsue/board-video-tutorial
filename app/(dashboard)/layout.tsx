70351b339acd902d9a0ac95105a1ed1d3dcbf98c
iinsue <hinsu93@gmail.com>
2024-03-10 12:42:53 +0900

    웹에서만 나타나는 사이드 바 생성

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Sidebar } from "./_components/sidebar";
+import { OrgSidebar } from "./_components/org-sidebar";
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -9,7 +10,12 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">{children}</div>
+        </div>
+      </div>
     </main>
   );
 };
