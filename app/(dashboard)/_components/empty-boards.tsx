d9ade9dba93dba500ccda43b7a399f3fd218d14f
iinsue <hinsu93@naver.com>
2024-04-02 23:14:30 +0900

    board 생성 기능 추가

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,26 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
+import { useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    create({
+      orgId: organization.id,
+      title: "Untitled",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
@@ -10,7 +29,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button size="lg" onClick={onClick}>
+          Create board
+        </Button>
       </div>
     </div>
   );
